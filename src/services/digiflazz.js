527175b73ab7cc8f1cfb5c22ccca8637966b6ce5
zackyhere <zackyy425@gmail.com>
2025-12-29 23:17:12 +0800

    add logo payment

--- src/services/digiflazz.js
+++ src/services/digiflazz.js
@@ -33,83 +33,37 @@ async function getProductList(product, required_form) {
             rekomendasi: {
                 qris: {
                     admin: "1500",
-                    svg: "<svg></svg>"
-                },
-                gopay: {
-                    admin: "1500",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/e/e0/QRIS_Logo.svg'></img>"
                 }
             },
 
             ewallet: {
                 dana: {
                     admin: "1000",
-                    svg: `<svg
-  width="64"
-  height="64"
-  viewBox="0 0 64 64"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <!-- Background -->
-  <circle cx="32" cy="32" r="32" fill="#00AED6"/>
-
-  <!-- Wallet shape -->
-  <path
-    d="M18 24
-       H40
-       C44 24 46 26 46 30
-       V36
-       C46 40 44 42 40 42
-       H18
-       C15 42 14 40 14 38
-       V28
-       C14 26 15 24 18 24
-       Z"
-    fill="white"
-  />
-
-  <!-- Wallet pocket -->
-  <rect
-    x="34"
-    y="30"
-    width="10"
-    height="8"
-    rx="2"
-    fill="#00AED6"
-  />
-
-  <!-- Button -->
-  <circle
-    cx="38"
-    cy="34"
-    r="1.5"
-    fill="white"
-  />
-</svg>
-`
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg'></img>"
                 },
                 shopeepay: {
                     admin: "1200",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://akmweb.youngjoygame.com/web/gms/image/694619ac9a3170bdeb9bc672ee1f8fc2.jpg'></img>"
                 },
                 gopay: {
                     admin: "1300",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg'></img>"
                 }
             },
 
             virtual_account: {
                 mandiri: {
                     admin: "4000",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg'></img>"
                 },
                 bri: {
                     admin: "3500",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg'></img>"
                 },
                 bni: {
                     admin: "3800",
-                    svg: "<svg></svg>"
+                    svg: "<img src='https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg'></img>"
                 }
             }
         },
@@ -126,58 +80,6 @@ async function getProductList(product, required_form) {
             {
                 name: "Produk C",
                 price: 10000
-            },
-            {
-                name: "Produk D",
-                price: 25000
-            },
-            {
-                name: "Produk E",
-                price: 10000
-            },
-            {
-                name: "Produk F",
-                price: 25000
-            },
-            {
-                name: "Produk G",
-                price: 10000
-            },
-            {
-                name: "Produk H",
-                price: 25000
-            },
-            {
-                name: "Produk I",
-                price: 10000
-            },
-            {
-                name: "Produk J",
-                price: 25000
-            },
-            {
-                name: "Produk K",
-                price: 10000
-            },
-            {
-                name: "Produk L",
-                price: 25000
-            },
-            {
-                name: "Produk M",
-                price: 10000
-            },
-            {
-                name: "Produk N",
-                price: 25000
-            },
-            {
-                name: "Produk O",
-                price: 10000
-            },
-            {
-                name: "Produk P",
-                price: 25000
             }
         ]
     };
